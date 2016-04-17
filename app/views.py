2278fc5ed16e47832d4c9983d7fd577c8c632f6e
Dustin Holman <holmandw11@gmail.com>
2016-04-17 15:44:49 -0400

    removed junnk from app/views

--- app/views.py
+++ app/views.py
@@ -12,22 +12,6 @@ def index():
                            temperature=temp.temperature,
                            units=temp.units.lower())
 
-"""
-@app.route('/historical')
-def stat_list():
-    stats = models.ServerStats.query.all()
-    items = reversed([tables.Item(
-        s.timestamp.strftime('%H:%M - %m/%d'),
-        '{:.2f}'.format(s.cpu_percent),
-        '{:.2f}'.format(s.mem_percent)) for s in stats])
-    table = tables.HistoricalTable(items, classes=['table'])
-    return render_template(
-        'stat_list.html',
-        server=platform.node(),
-        stats=reversed(stats[-10:]),
-        table=table,
-    )
-"""
 
 @app.errorhandler(404)
 def not_found_error(error):
